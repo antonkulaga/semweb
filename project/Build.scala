74588e42ad3cf17880c3a5fa3794ab9fce6dca67
Anton Kulaga <antonkulaga@gmail.com>
2014-08-16 22:47:58 +0300

    bump version to 0.6.5

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object Build extends sbt.Build{
 
  def repo = if(isRelease) "scalax-releases" else "scalax-snapshots"
 
- val semWebVer = "0.6.4"
+ val semWebVer = "0.6.5"
 
  publishMavenStyle := false
 
